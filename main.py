6b8bd13229d303155ee86a43b5607af7ce65ad1e
ModernMon <lakshmiaravind.atom@gmail.com>
2023-06-15 08:51:10 +0530

    CREATED GRAPHS DYNAMICALLY WITH FAKE DATA(USING PLOTLY)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import plotly.express
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather forecast for next few days...")
 
@@ -10,4 +12,15 @@ data = st.selectbox("Select data to View", ('Sky','Temperature'))
 
 st.subheader(f"{data} for next {days} Days in {place} :")
 
+def get_dates(days):
+    temperature = [.1, 10, 15]
+    dates = ["27-10-96", '27-11-96', '28-10-96']
+    temperature = [days * i for i in temperature]
+    return dates, temperature
 
+d, t = get_dates(days)
+
+
+
+figure = plotly.express.line(x=d, y=t ,labels={"x":"Dates","y":"Temperature (C)"})
+st.plotly_chart(figure)
